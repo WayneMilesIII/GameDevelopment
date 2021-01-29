f147a9ddee45a60b94a8c96995e01fa24eca76c8
midnight1maniac2 <78062038+midnight1maniac2@users.noreply.github.com>
2021-01-28 18:44:02 -0800

    Add files via upload

---
+++ Projectile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Projectile
+{
+	private int cycles;
+	private int damageDealt;
+	protected final int xOrigin, yOrigin;
+	protected double angle;
+	protected Sprite sprite;
+	protected double nx, ny;
+	protected double speed, rateOfFire, range, damage;
+
+
+
+	private const int TICKS = 2;
+
+	// damage counter
+	public void damageCounter
+    {
+		damageDealt = health - 1;
+
+    }
+
+
+	public Projectile(int x, int y, int dir)
+	{
+		Console.WriteLine("Hi");
+		xOrigin = x;
+		yOrigin = y;
+		angle = dir;
+
+		cycles = 2;
+		damageDealt = 1;
+	}
+}
