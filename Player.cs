4ddea8a5f012b0969ce71b255e760ad28b07dd0d
preppeh <78000759+preppeh@users.noreply.github.com>
2021-01-28 18:32:48 -0800

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Player
+{
+	private int lives;
+	private int health;
+	public Player()
+	{
+		lives = 3;
+		health = 3;
+	}
+	
+	public void loseHealth(Enemy x)
+    {
+		health = health - x.getDamage();
+		if(health == 0)
+        {
+			lives = lives - 1;
+			health = 3;
+        }
+		if(lives == 0)
+        {
+			gameOver();
+        }
+    }
+	public void shoot()
+    {
+
+    }
+	public void movement()
+    {
+
+    }
+	public void gameOver()
+    {
+
+    }
+}
