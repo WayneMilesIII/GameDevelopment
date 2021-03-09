d0bd98e305a04ce4960e7ca8467e2733d4f9a284
T-Bunz20 <56098144+T-Bunz20@users.noreply.github.com>
2021-03-09 14:48:45 -0800

    Update HealthBar.cs

--- Script/HealthBar.cs
+++ Script/HealthBar.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public float CurrentHealth { get; set; }
+    public float MaxHealth { get; set; }
 
     public Slider slider;
+    public Player player;
 
     public void SetMaxHealth(int health)
     {
@@ -19,5 +22,39 @@ public class HealthBar : MonoBehaviour
         slider.value = health;
     }
 
+    void Start()
+    {
+        MaxHealth = 20f;
 
+        CurrentHealth = MaxHealth;
+
+        slider.value = CalculateHealth();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            DealDamage(10);
+        }
+    }
+
+    void DealDamage(float damageValue)
+    {
+        CurrentHealth -= damageValue;
+
+        if (CurrentHealth <= 0)
+            Die();
+    }
+
+    float CalculateHealth()
+    {
+        return CurrentHealth / MaxHealth;
+    }
+
+    void Die()
+    {
+        CurrentHealth = 0;
+        
+    }
 }
