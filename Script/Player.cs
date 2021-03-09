cbbb43ea221007f861a6cf6643eb257bb347e63b
T-Bunz20 <56098144+T-Bunz20@users.noreply.github.com>
2021-03-08 17:41:47 -0800

    Update Player.cs

--- Script/Player.cs
+++ Script/Player.cs
@@ -17,8 +17,10 @@ public class Player : MonoBehaviour
     public GameObject projTemplate;
     //Reference the Game Over Screen
     public GameOverScreen gameOverScreen;
-    //Reference to Status
-    public Status status;
+    //Reference for player spawning
+    public Transform spawnPoint;
+    //Reference to PauseMenu
+    public PauseMenu pause;
 
     /*
      * Variables
@@ -29,10 +31,24 @@ public class Player : MonoBehaviour
     public float projForce = 20f;
     //Variable for player health
     public static int health;
+    public int maxHealth = 50;
+    //Variable for player current health
+    public int currentHealth;
     //Variable for player lives
     public static int lives;
     //Boolean for player death
     public static bool isDead;
+    //Boolean for I-Frames
+    private bool isInvincible;
+    //How long player will be invincible
+    [SerializeField]
+    private float invincibilityDurationSeconds;
+    //Time between player flashes
+    [SerializeField]
+    private float delayBetweenInvincibilityFlashes;
+    //Model to scale up and down for flashes
+    [SerializeField]
+    private GameObject model;
 
     /*
      * Other
@@ -41,12 +57,24 @@ public class Player : MonoBehaviour
     Vector2 movement;
     Vector2 mousePos;
 
+    // health bar and life counter
+    public HealthBar healthBar;
+    public LifeCounter lifeCounter;
+
+    public GameObject life1, life2, life3;
+
     // Start is called before the first frame update
     void Start()
     {
-        health = 100;
-        lives = 5;
+        healthBar.SetMaxHealth(maxHealth);
+        health = maxHealth;
+        lives = 3;
+        life1.gameObject.SetActive(true);
+        life2.gameObject.SetActive(true);
+        life3.gameObject.SetActive(true);
+        Status.playerScore = 0;
         isDead = false;
+        pause.Resume();
     }
 
     // Update is called once per frame
@@ -58,17 +86,37 @@ public class Player : MonoBehaviour
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         PlayerDeathCheck();
-        if (PauseMenu.GameIsPaused == false)
+        if (lives > 3)
+            lives = 3;
+        switch (lives)
+        {
+            case 3:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(true);
+                break;
+            case 2:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(true);
+                life3.gameObject.SetActive(false);
+                break;
+            case 1:
+                life1.gameObject.SetActive(true);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                break;
+            case 0:
+                life1.gameObject.SetActive(false);
+                life2.gameObject.SetActive(false);
+                life3.gameObject.SetActive(false);
+                break;
+        }
+        if (!PauseMenu.GameIsPaused)
         {
             if (Input.GetButtonDown("Fire1"))
             {
                 Shoot();
             }
-        }
-
-        if (Input.GetKeyDown("escape"))
-        {
-            Application.Quit();
         }
     }
 
@@ -82,6 +130,15 @@ public class Player : MonoBehaviour
         rb.rotation = angle;
     }
 
+    // method that will take the damge
+    void TakeDamage(int damage)
+    {
+        
+        health -= damage;
+        healthBar.SetHealth(health);
+        StartCoroutine(BecomeTemporarilyInvincible());
+    }
+
     void Shoot()
     {
         GameObject projectile = Instantiate(projTemplate, Projectile_Spawner.position, Projectile_Spawner.rotation);
@@ -91,12 +148,40 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.gameObject.tag.Equals("Enemy"))
+        if (col.gameObject.tag.Equals("Enemy") && !isInvincible)
         {
-            health -= 10;
+            TakeDamage(10);
             PlayerDeathCheck();
             //DO SOMETHING TO MOVE ENEMY AWAY FROM PLAYER AFTER CONTACT
         }
+    }
+
+    private void ScaleModelTo(Vector3 scale)
+    {
+        model.transform.localScale = scale;
+    }
+
+    private IEnumerator BecomeTemporarilyInvincible()
+    {
+        Debug.Log("Player turned invincible!");
+        isInvincible = true;
+
+        // Flash on and off for roughly invincibilityDurationSeconds seconds
+        for (float i = 0; i < invincibilityDurationSeconds; i += delayBetweenInvincibilityFlashes)
+        {
+            if (model.transform.localScale == Vector3.one)
+            {
+                ScaleModelTo(Vector3.zero);
+            }
+            else
+            {
+                ScaleModelTo(Vector3.one);
+            }
+            yield return new WaitForSeconds(delayBetweenInvincibilityFlashes);
+        }
+        ScaleModelTo(Vector3.one);
+        Debug.Log("Player is no longer invincible!");
+        isInvincible = false;
     }
 
     void PlayerDeathCheck()
@@ -104,16 +189,18 @@ public class Player : MonoBehaviour
         if (health <= 0)
         {
             isDead = true;
-            health += 100;
+            health += 50;
+            healthBar.SetMaxHealth(maxHealth);
             lives -= 1;
         }
-        if(lives <= 0) 
+        if (lives <= 0)
         {
-            gameOverScreen.Setup(status.getScore());
-        } else if(isDead)
+            gameOverScreen.Setup(Status.getScore());
+        }
+        else if (isDead)
         {
             isDead = false;
-            transform.position = spawnPoint.position; 
+            transform.position = spawnPoint.position;
         }
     }
 }
